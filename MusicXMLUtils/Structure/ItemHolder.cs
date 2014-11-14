0bf3fb968df67de04fedc2d1cf8a7ed9ca1e70fa
Tomona Nanase <nanase@users.noreply.github.com>
2014-11-14 14:56:40 +0900

    ItemHolderクラスを追加

---
+++ MusicXMLUtils/Structure/ItemHolder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace MusicXMLUtils.Structure
+{
+    public abstract class ItemHolder<T>
+    {
+        private List<Tuple<T, object>> items = null;
+
+        [XmlIgnore]
+        public abstract object[] Items { get; set; }
+
+        [XmlIgnore]
+        public abstract T[] ItemsElementName { get; set; }
+
+        public void InitalizeItems()
+        {
+            if (this.items == null)
+                this.items = new List<Tuple<T, object>>();
+            else
+                this.items.Clear();
+        }
+
+        public void AddItem(T elementName, object item)
+        {
+            if (this.items == null)
+                this.InitalizeItems();
+
+            this.items.Add(new Tuple<T, object>(elementName, item));
+        }
+
+        public void ApplyItems()
+        {
+            if (this.items == null)
+                this.InitalizeItems();
+
+            int count = this.items.Count;
+            this.Items = new object[count];
+            this.ItemsElementName = new T[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var item = this.items[i];
+                this.Items[i] = item.Item2;
+                this.ItemsElementName[i] = item.Item1;
+            }
+
+            this.items.Clear();
+            this.items = null;
+        }
+    }
+}
