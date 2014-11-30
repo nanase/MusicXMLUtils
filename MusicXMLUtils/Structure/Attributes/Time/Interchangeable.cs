353ed9f7725ccb72268da70eda9a39da251138e7
Tomona Nanase <nanase@users.noreply.github.com>
2014-11-30 23:05:15 +0900

    Interchangeableクラスにregionを追加

--- MusicXMLUtils/Structure/Attributes/Time/Interchangeable.cs
+++ MusicXMLUtils/Structure/Attributes/Time/Interchangeable.cs
@@ -38,6 +38,7 @@ namespace MusicXMLUtils.Structure
     [XmlType("interchangeable")]
     public class Interchangeable
     {
+        #region -- Public Properties --
         [XmlElement("time-relation")]
         public TimeRelation TimeRelation { get; set; }
 
@@ -61,5 +62,6 @@ namespace MusicXMLUtils.Structure
 
         [XmlIgnore]
         public bool SeparatorSpecified { get; set; }
+        #endregion
     }
 }
