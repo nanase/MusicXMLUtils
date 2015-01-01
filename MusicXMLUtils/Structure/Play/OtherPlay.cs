4246c9cc7779dcf0ecd39573a806d29d6a83ac94
Tomona Nanase <nanase@users.noreply.github.com>
2015-01-01 22:26:13 +0900

    OtherPlayクラスにregionを追加

--- MusicXMLUtils/Structure/Play/OtherPlay.cs
+++ MusicXMLUtils/Structure/Play/OtherPlay.cs
@@ -38,10 +38,12 @@ namespace MusicXMLUtils.Structure
     [XmlType(TypeName = "other-play")]
     public class OtherPlay
     {
+        #region -- Public Properties --
         [XmlAttribute("type", DataType = "token")]
         public string Type { get; set; }
 
         [XmlText]
         public string Value { get; set; }
+        #endregion
     }
 }
